afbdfd4b28941210d69db8bf58925d80cd59eaf1
kchalkowski <kzc0061@auburn.edu>
2025-01-13 10:51:29 -0700

    updating model output tidying in speed/distance models

--- 2_CTMM_Pipeline/4_Fine_Scale_Mvmt_GLMMs.R
+++ 2_CTMM_Pipeline/4_Fine_Scale_Mvmt_GLMMs.R
@@ -183,21 +183,21 @@ mesh_cutoff=1
 distaer$mX_sc <- floor(distaer$mX/spatial_res)
 distaer$mY_sc <- floor(distaer$mY/spatial_res)
 meshtox_sp <- make_mesh(distaer,c("mX_sc","mY_sc"),cutoff=mesh_cutoff)
-res_dist_rp_aer=sdmTMB(weekly_dist_km ~ (1|animalid) + trt_ctrl*removal.period.akdecalc,
+res_distance_rp_aer=sdmTMB(weekly_dist_km ~ (1|animalid) + trt_ctrl*removal.period.akdecalc,
                         data=distaer,
                         mesh=meshtox_sp,
                         spatial='on',
                         family=Gamma(link='log'))
 
-sanity(res_dist_rp_aer)
-tox_res_rp <- simulate(res_dist_rp_aer, nsim = 544, type = "mle-mvn") %>% 
-  dharma_residuals(res_dist_rp_aer, return_DHARMa = TRUE)
+sanity(res_distance_rp_aer)
+tox_res_rp <- simulate(res_distance_rp_aer, nsim = 544, type = "mle-mvn") %>% 
+  dharma_residuals(res_distance_rp_aer, return_DHARMa = TRUE)
 tox_res_rp2 = recalculateResiduals(tox_res_rp, group = as.factor(distaer$animalid),rotation="estimated")
 groupLocations = aggregate(distaer[,c("mX","mY")], list(distaer$animalid), mean)
 testSpatialAutocorrelation(tox_res_rp2,groupLocations$mX,groupLocations$mY)
 #removed spatial autocorrelation, p=0.931
 
-saveRDS(res_dist_rp_aer,paste0(results_dir,"res_distance_rp_aer.rds"))
+#saveRDS(res_dist_rp_aer,paste0(results_dir,"res_distance_rp_aer.rds"))
 
 #### removal type * period * sex ------
 
@@ -205,7 +205,7 @@ res_distance_rps_aer <-glmmTMB(weekly_dist_km ~ (1|animalid) +
                                  trt_ctrl*removal.period.akdecalc*sex,
                                data=distaer,family=Gamma(link="log"))
 
-saveRDS(res_distance_rps_aer,paste0(results_dir,"res_distance_rps_aer.rds"))
+#saveRDS(res_distance_rps_aer,paste0(results_dir,"res_distance_rps_aer.rds"))
 
 ### trap -----------------
 
@@ -215,14 +215,14 @@ res_distance_rp_trap=glmmTMB(weekly_dist_km ~ trt_ctrl*removal.period.akdecalc+
                                (1|animalid),
                              data=disttrap,family=Gamma(link="log"))
 
-saveRDS(res_distance_rp_trap,paste0(results_dir,"res_distance_rp_trap.rds"))
+#saveRDS(res_distance_rp_trap,paste0(results_dir,"res_distance_rp_trap.rds"))
 
 #### removal type * period * sex ------
 res_distance_rps_trap=glmmTMB(weekly_dist_km ~ trt_ctrl*removal.period.akdecalc*sex+
                                 (1|animalid),
                               data=disttrap,family=Gamma(link="log"))
 
-saveRDS(res_distance_rps_trap,paste0(results_dir,"res_distance_rps_trap.rds"))
+#saveRDS(res_distance_rps_trap,paste0(results_dir,"res_distance_rps_trap.rds"))
 
 ### toxicant -----------------
 
@@ -238,7 +238,7 @@ res_distance_rp_tox=sdmTMB(weekly_dist_km ~ (1|animalid) + trt_ctrl*removal.peri
                        spatial='on',
                        family=Gamma(link='log'))
 
-sanity(res_dist_rp_aer)
+sanity(res_distance_rp_tox)
 tox_res_rp <- simulate(res_distance_rp_tox, nsim = 544, type = "mle-mvn") %>% 
   dharma_residuals(res_distance_rp_tox, return_DHARMa = TRUE)
 tox_res_rp2 = recalculateResiduals(tox_res_rp, group = as.factor(disttox$animalid),rotation="estimated")
@@ -246,14 +246,14 @@ groupLocations = aggregate(disttox[,c("mX","mY")], list(disttox$animalid), mean)
 testSpatialAutocorrelation(tox_res_rp2,groupLocations$mX,groupLocations$mY)
 #removed spatial autocorrelation, p=0.106
 
-saveRDS(res_distance_rp_tox,paste0(results_dir,"res_distance_rp_tox.rds"))
+#saveRDS(res_distance_rp_tox,paste0(results_dir,"res_distance_rp_tox.rds"))
 
 #### removal type * period * sex ------
-res_distance_rps_tox=glmmTMB(weekly_dist_km ~ trt_ctrl*removal.period.akdecalc+
+res_distance_rps_tox=glmmTMB(weekly_dist_km ~ trt_ctrl*removal.period.akdecalc*sex+
                                (1|animalid),
                              data=disttox,family=Gamma(link="log"))
 
-saveRDS(res_distance_rps_tox,paste0(results_dir,"res_distance_rps_tox.rds"))
+#saveRDS(res_distance_rps_tox,paste0(results_dir,"res_distance_rps_tox.rds"))
 
 #speed -------------------
 
@@ -337,7 +337,7 @@ res_speed_rp_aer <- glmmTMB(weekly_md_km_hr ~ trt_ctrl*removal.period.akdecalc+
                                family=Gamma(link="log"),
                                verbose=TRUE)
 
-saveRDS(res_speed_rp_aer,paste0(results_dir,"res_speed_rp_aer.rds"))
+#saveRDS(res_speed_rp_aer,paste0(results_dir,"res_speed_rp_aer.rds"))
 
 #### removal type * period * sex ------
 res_speed_rps_aer <- glmmTMB(weekly_md_km_hr ~ trt_ctrl*removal.period.akdecalc*sex+
@@ -346,7 +346,7 @@ res_speed_rps_aer <- glmmTMB(weekly_md_km_hr ~ trt_ctrl*removal.period.akdecalc*
                             family=Gamma(link="log"),
                             verbose=TRUE)
 
-saveRDS(res_speed_rps_aer,paste0(results_dir,"res_speed_rps_aer.rds"))
+#saveRDS(res_speed_rps_aer,paste0(results_dir,"res_speed_rps_aer.rds"))
 
 ### trap ----------------
 #### removal type * period ------
@@ -355,7 +355,7 @@ res_speed_rp_trap=glmmTMB(weekly_md_km_hr~
                             trt_ctrl*removal.period.akdecalc,
                           data=speedtrap,family=Gamma(link='log'))
 
-saveRDS(res_speed_rp_trap,paste0(results_dir,"res_speed_rp_trap.rds"))
+#saveRDS(res_speed_rp_trap,paste0(results_dir,"res_speed_rp_trap.rds"))
 
 #### removal type * period * sex ------
 res_speed_rps_trap=glmmTMB(weekly_md_km_hr~ 
@@ -363,7 +363,7 @@ res_speed_rps_trap=glmmTMB(weekly_md_km_hr~
                              trt_ctrl*removal.period.akdecalc*sex,
                            data=speedtrap,family=Gamma(link='log'))
 
-saveRDS(res_speed_rps_trap,paste0(results_dir,"res_speed_rps_trap.rds"))
+#saveRDS(res_speed_rps_trap,paste0(results_dir,"res_speed_rps_trap.rds"))
 
 ### toxicant ----------------
 #set mesh cutoff for spatial models
@@ -388,7 +388,7 @@ groupLocations = aggregate(speedtox[,c("mX","mY")], list(speedtox$animalid), mea
 testSpatialAutocorrelation(tox_res_rp2,groupLocations$mX,groupLocations$mY)
 #removed spatial autocorrelation, p=0.5807
 
-saveRDS(res_speed_rp_tox,paste0(results_dir,"res_speed_rp_tox.rds"))
+#saveRDS(res_speed_rp_tox,paste0(results_dir,"res_speed_rp_tox.rds"))
 
 #### removal type * period * sex ------
 meshtox_sp <- make_mesh(speedtox,c("mX_sc","mY_sc"),cutoff=mesh_cutoff)
@@ -406,7 +406,197 @@ groupLocations = aggregate(speedtox[,c("mX","mY")], list(speedtox$animalid), mea
 testSpatialAutocorrelation(tox_res_rp2,groupLocations$mX,groupLocations$mY)
 #removed spatial autocorrelation, p=0.6789
 
-saveRDS(res_speed_rps_tox,paste0(results_dir,"res_speed_rps_tox.rds"))
+#saveRDS(res_speed_rps_tox,paste0(results_dir,"res_speed_rps_tox.rds"))
+
+#! Note for Abbey ------------------------------
+# Changing output formatting to be in scripts where models are run, too difficult formatting output in separate script. 
+  # When contact models are finished, move these sections to below those glmms and add model objects to 'mods' and model names to 'models' as strings
+  # This formats the output to be used to make parameter tables, figures, etc.
+
+# Format model info -----------------------------------------------------------
+mods<-list(res_distance_rp_aer,
+           res_distance_rps_aer,
+           res_distance_rp_tox,
+           res_distance_rps_tox,
+           res_distance_rp_trap,
+           res_distance_rps_trap,
+           res_speed_rp_aer,
+           res_speed_rps_aer,
+           res_speed_rp_tox,
+           res_speed_rps_tox,
+           res_speed_rp_trap,
+           res_speed_rps_trap)
+
+models=c("res_distance_rp_aer",
+            "res_distance_rps_aer",
+            "res_distance_rp_tox",
+            "res_distance_rps_tox",
+            "res_distance_rp_trap",
+            "res_distance_rps_trap",
+            "res_speed_rp_aer",
+            "res_speed_rps_aer",
+            "res_speed_rp_tox",
+            "res_speed_rps_tox",
+            "res_speed_rp_trap",
+            "res_speed_rps_trap")
+
+# * Format model prediction df -----------
+for(i in 1:length(mods)){
+  #i=1
+  #i=8
+  #if(class(mods[[i]])=="glmmTMB"){
+  #coefs=summary(mods[[i]])$coef$cond
+  if(length(grep("rps",models[i]))==0){
+  tmp=as.data.frame(ggeffects::predict_response(mods[[i]], terms=c("trt_ctrl","removal.period.akdecalc")))
+    tmp$facet=NA
+  } else{
+    tmp=as.data.frame(ggeffects::predict_response(mods[[i]], terms=c("trt_ctrl","removal.period.akdecalc","sex")))
+  }
+  
+  #}
+  
+  tmp$model=models[i]
+  
+  if(i==1){
+    preds=tmp
+  } else{
+    preds=rbind(preds,tmp)
+  }
+  
+}
+
+#make colnames more informative
+colnames(preds)[c(1,6,7)]<-c("trt","per","sex")
+
+#Make rem column based on model
+preds$rem=NA
+preds$rem[grep("aer",preds$model)]<-"aer"
+preds$rem[grep("trap",preds$model)]<-"trap"
+preds$rem[grep("tox",preds$model)]<-"tox"
+
+#make response column
+preds$response=NA
+preds$response[grep("speed",preds$model)]<-"speed"
+preds$response[grep("distance",preds$model)]<-"distance"
+
+# Pull interactions table ------------------------------------------------------
+
+for(i in 1:length(mods)){
+  
+  if(class(mods[[i]])=="sdmTMB"){
+  df=broom.mixed::tidy(mods[[i]]) %>% as.data.frame()
+  z=df$estimate/df$std.error
+  df$p=exp(-0.717*z-0.416*z^2)
+  df$model=models[i]
+  df=df[,c(2,3,4,5,1)]
+  df=as.data.frame(df[grep(":",df$term),])
+  colnames(df)=c("Estimate","Std. Error","Pr(>|z|)","model","effect")
+  
+  } else{
+    #Pull coefficients
+    coefs=summary(mods[[i]])$coef$cond
+    
+    #Make table with just interaction effect, std.error, p value
+    coefs2=as.data.frame(coefs[grep(":",rownames(coefs)),c(1,2,4),drop=FALSE])
+    if(nrow(coefs2)!=0){
+      
+      if(nrow(coefs2)>1){
+        coefs2=as.data.frame(coefs2[grep("trt_ctrl",rownames(coefs2)),,drop=FALSE])
+        coefs2=as.data.frame(coefs2[grep("period",rownames(coefs2)),,drop=FALSE])
+      }
+      
+      
+      coefs2$model=models[i]
+      coefs2$effect=rownames(coefs2)
+      rownames(coefs2)=NULL
+      
+      #rename to rbind with sdmTMB results
+      df=coefs2
+    }
+  }
+  
+  if(i==1){
+    allc=df
+  } else{
+    allc=rbind(allc,df)
+  }
+}
+
+#check that all captured
+all(models%in%allc$model)
+#which(!(models%in%allc$model))
+
+#period
+allc$period=NA
+allc$period[grep("after",allc$effect)]<-"after"
+allc$period[grep("during",allc$effect)]<-"during"
+
+#remove NA for period, means is intxn between treatment and sex alone
+allc=allc[!is.na(allc$period),]
+
+#sex
+allc$sex=NA
+allc$sex[grep("rps",allc$model)]<-"female"
+allc$sex[grep("sexMale",allc$effect)]<-"male"
+
+#get treatment type
+allc$trt=NA
+allc$trt[grep("aer",allc$model)]<-"aer"
+allc$trt[grep("trap",allc$model)]<-"trap"
+allc$trt[grep("tox",allc$model)]<-"tox"
+
+#get response type
+allc$response=NA
+allc$response[grep("speed",allc$model)]<-"speed"
+allc$response[grep("distance",allc$model)]<-"distance"
+
+#Remove ugly effect column
+allc=allc[,-which(colnames(allc)=="effect")]
+
+#change sex NA
+allc$sex[is.na(allc$sex)]<-"whole"
+
+#subset to only significant interactions
+allc$alpha=0.1
+allc$alpha[allc$`Pr(>|z|)`<0.05]<-1
+
+# Combine full parameter tables -----------------------------------------------------------
+for(i in 1:length(mods)){
+  if(class(mods[[i]])=="sdmTMB"){
+  df=broom.mixed::tidy(mods[[i]])
+  df$statistic=df$estimate/df$std.error
+  df$p.value=exp(-0.717*df$statistic-0.416*df$statistic^2)
+  df<-as.data.frame(df)
+  }
+  
+  #add statistic, p value
+  if(class(mods[[i]])=="glmmTMB"){
+    df=broom.mixed::tidy(mods[[i]])
+    df$model=models[i]
+    df=df[df$effect=="fixed",]
+    #term, estimate, std error, statistic, p value
+    df=df[,c(4:8)]
+    df<-as.data.frame(df)
+    
+  }
+  
+  df$model=models[i]
+  
+  if(i==1){
+    parms=df
+  } else{
+    parms=rbind(parms,df)
+  }
+  
+}
+
+# Save tidied model output ----------------
+outdir=file.path(homedir,"3_Output",fsep=.Platform$file.sep)
+if(!dir.exists(file.path(outdir,"Model_Output"))){dir.create(file.path(outdir,"Model_Output"))}
+saveRDS(preds,file.path(outdir,"Model_Output","spdist_preds.rds"))
+saveRDS(allc,file.path(outdir,"Model_Output","spdist_intxns.rds"))
+saveRDS(parms,file.path(outdir,"Model_Output","spdist_full_param.rds"))
+
 
 # contacts - number --------
 parContacts <- readRDS(paste0(objdir,"/pairwise_contacts.rds"))
@@ -444,7 +634,7 @@ plot(aer_res_ncon_rp2$simulatedResponse)
 descdist(aer_res_ncon_rp2$fittedResiduals)
 DHARMa::testDispersion(aer_res_ncon_rp2)
 
-saveRDS(res_ncon_rp_aer,paste0(results_dir,"res_ncon_rp_aer.rds"))
+#saveRDS(res_ncon_rp_aer,paste0(results_dir,"res_ncon_rp_aer.rds"))
 
 ## removal type * period *sex -----
 res_ncon_rps_aer=glmmTMB(num_contacts~(1|animalid)+
@@ -461,7 +651,7 @@ plot(aer_res_ncon_rps2$simulatedResponse)
 descdist(aer_res_ncon_rps2$fittedResiduals)
 DHARMa::testDispersion(aer_res_ncon_rps2)
 
-saveRDS(res_ncon_rps_aer,paste0(results_dir,"res_ncon_rps_aer.rds"))
+#saveRDS(res_ncon_rps_aer,paste0(results_dir,"res_ncon_rps_aer.rds"))
 
 #trap ------
 ## removal type * period -----
@@ -479,7 +669,7 @@ plot(trap_res_ncon_rp2$simulatedResponse)
 descdist(trap_res_ncon_rp2$fittedResiduals)
 DHARMa::testDispersion(trap_res_ncon_rp2)
 
-saveRDS(res_ncon_rp_trap,paste0(results_dir,"res_ncon_rp_trap.rds"))
+#saveRDS(res_ncon_rp_trap,paste0(results_dir,"res_ncon_rp_trap.rds"))
 
 ## removal type * period *sex -----
 res_ncon_rps_trap=glmmTMB(num_contacts~(1|animalid)+
@@ -496,7 +686,7 @@ plot(trap_res_ncon_rps2$simulatedResponse)
 descdist(trap_res_ncon_rps2$fittedResiduals)
 DHARMa::testDispersion(trap_res_ncon_rps2)
 
-saveRDS(res_ncon_rps_trap,paste0(results_dir,"res_ncon_rps_trap.rds"))
+#saveRDS(res_ncon_rps_trap,paste0(results_dir,"res_ncon_rps_trap.rds"))
 
 #tox ------
 ## removal type * period -----
@@ -514,7 +704,7 @@ plot(tox_res_ncon_rp2$simulatedResponse)
 descdist(tox_res_ncon_rp2$fittedResiduals)
 DHARMa::testDispersion(tox_res_ncon_rp2)
 
-saveRDS(res_ncon_rp_tox,paste0(results_dir,"res_ncon_rp_tox.rds"))
+#saveRDS(res_ncon_rp_tox,paste0(results_dir,"res_ncon_rp_tox.rds"))
 
 ## removal type * period *sex -----
 res_ncon_rps_tox=glmmTMB(num_contacts~(1|animalid)+
@@ -531,7 +721,7 @@ plot(tox_res_ncon_rps2$simulatedResponse)
 descdist(tox_res_ncon_rps2$fittedResiduals)
 DHARMa::testDispersion(tox_res_ncon_rps2)
 
-saveRDS(res_ncon_rps_tox,paste0(results_dir,"res_ncon_rps_tox.rds"))
+#saveRDS(res_ncon_rps_tox,paste0(results_dir,"res_ncon_rps_tox.rds"))
 
 
 # contacts - degree --------
@@ -552,7 +742,7 @@ plot(aer_res_nind_rp2$simulatedResponse)
 descdist(aer_res_nind_rp2$fittedResiduals)
 DHARMa::testDispersion(aer_res_nind_rp2)
 
-saveRDS(res_nind_rp_aer,paste0(results_dir,"res_nind_rp_aer.rds"))
+#saveRDS(res_nind_rp_aer,paste0(results_dir,"res_nind_rp_aer.rds"))
 
 ## removal type * period *sex -----
 res_nind_rps_aer=glmmTMB(num_indivs~(1|animalid)+
@@ -569,7 +759,7 @@ plot(aer_res_nind_rps2$simulatedResponse)
 descdist(aer_res_nind_rps2$fittedResiduals)
 DHARMa::testDispersion(aer_res_nind_rps2)
 
-saveRDS(res_nind_rps_aer,paste0(results_dir,"res_nind_rps_aer.rds"))
+#saveRDS(res_nind_rps_aer,paste0(results_dir,"res_nind_rps_aer.rds"))
 
 #trap ------
 ## removal type * period -----
@@ -587,7 +777,7 @@ plot(trap_res_nind_rp2$simulatedResponse)
 descdist(trap_res_nind_rp2$fittedResiduals)
 DHARMa::testDispersion(trap_res_nind_rp2)
 
-saveRDS(res_nind_rp_trap,paste0(results_dir,"res_nind_rp_trap.rds"))
+#saveRDS(res_nind_rp_trap,paste0(results_dir,"res_nind_rp_trap.rds"))
 
 ## removal type * period *sex -----
 res_nind_rps_trap=glmmTMB(num_indivs~(1|animalid)+
@@ -604,7 +794,7 @@ plot(trap_res_nind_rps2$simulatedResponse)
 descdist(trap_res_nind_rps2$fittedResiduals)
 DHARMa::testDispersion(trap_res_nind_rps2)
 
-saveRDS(res_nind_rps_trap,paste0(results_dir,"res_nind_rps_trap.rds"))
+#saveRDS(res_nind_rps_trap,paste0(results_dir,"res_nind_rps_trap.rds"))
 
 #tox ------
 ## removal type * period -----
@@ -622,7 +812,7 @@ plot(tox_res_nind_rp2$simulatedResponse)
 descdist(tox_res_nind_rp2$fittedResiduals)
 DHARMa::testDispersion(tox_res_nind_rp2)
 
-saveRDS(res_nind_rp_tox,paste0(results_dir,"res_nind_rp_tox.rds"))
+#saveRDS(res_nind_rp_tox,paste0(results_dir,"res_nind_rp_tox.rds"))
 
 ## removal type * period *sex -----
 res_nind_rps_tox=glmmTMB(num_indivs~(1|animalid)+
@@ -639,5 +829,5 @@ plot(tox_res_nind_rps2$simulatedResponse)
 descdist(tox_res_nind_rps2$fittedResiduals)
 DHARMa::testDispersion(tox_res_nind_rps2)
 
-saveRDS(res_nind_rps_tox,paste0(results_dir,"res_nind_rps_tox.rds"))
+#saveRDS(res_nind_rps_tox,paste0(results_dir,"res_nind_rps_tox.rds"))
 
